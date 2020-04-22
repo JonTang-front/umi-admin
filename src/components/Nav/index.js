7abd2f7f0d31e1e88a95767ebc2693c6883ec59c
tangxiaoxin <1302947749@qq.com>
2020-04-22 14:34:16 +0800

    路由配置

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,15 +1,21 @@
+import React, { Component } from 'react';
 import NavLink from 'umi/navlink';
 import withRouter from 'umi/withRouter';
-import { useState, useEffect } from 'react';
 import { Menu, Icon } from "antd";
 import menuConfig from "../../config/menuConfig.js";
 import style from './index.less';
 
 const { SubMenu, Item } = Menu;
-function Nav(props) {
-    const [pathname, setPathname] = useState(null);
-    // const [menuNodeTree, setMenuNodeTree] = useState(null);
-    const renderMenu = menuConfig => {
+class Nav extends Component{
+    state = {}
+    componentWillMount() {
+        const menuNodeTree = this.renderMenu(menuConfig);
+        this.setState({
+            menuNodeTree,
+            pathname: this.props.location.pathname
+        });
+    }
+    renderMenu = menuConfig => {
         return menuConfig.map((item) => {
             if(item.children){
                 return (
@@ -20,7 +26,7 @@ function Nav(props) {
                                 </span>
                             } 
                             key={item.key}>
-                        { renderMenu(item.children) }
+                        { this.renderMenu(item.children) }
                     </SubMenu>
                 );
             }
@@ -41,22 +47,19 @@ function Nav(props) {
             );
         });
     }
-    const menuNodeTree = renderMenu(menuConfig);
-    useEffect(() => {
-        // setMenuNodeTree(renderMenu(menuConfig));
-        setPathname(props.location.pathname);
-    });
-    return (
-        <div className={style.nav_wrapper}>
-            <div className={style.logo}>
-                <img src={require('./../../assets/logo.svg')} alt="logo"/>
-                {props.collapsed? '' : <h1>Admin</h1>}
+    render() {
+        return (
+            <div className={style.nav_wrapper}>
+                <div className={style.logo}>
+                    <img src={require('./../../assets/logo.svg')} alt="logo"/>
+                    {this.props.collapsed? '' : <h1>Admin</h1>}
+                </div>
+                <Menu theme="dark" defaultSelectedKeys={[this.state.pathname]}>
+                    { this.state.menuNodeTree }
+                </Menu>
             </div>
-            <Menu theme="dark" defaultSelectedKeys={[pathname]}>
-                { menuNodeTree }
-            </Menu>
-        </div>
-    );
+        );
+    }
 }
 
 export default withRouter(Nav);

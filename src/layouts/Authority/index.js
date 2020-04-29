68dd1281c92aa174912e39227e88326b5c46ef5f
tangxiaoxin <1302947749@qq.com>
2020-04-29 17:54:07 +0800

    添加yarml注释路由

---
+++ src/layouts/Authority/index.js
@@ -0,0 +1,24 @@
+// import router from 'umi/router';
+// import BaseLayout from './../BaseLayout';
+// import Login from './../../pages/login';
+// import Util from './../../util';
+
+// const { handleLocalStorage } = Util;
+// export default function(props){
+//     const token = handleLocalStorage.getItem('_TOKEN');
+//     if(token){
+//         return (
+//             <BaseLayout>
+//                 { props.children }
+//             </BaseLayout>
+//         );
+//     }else{
+//         router.push('/login');
+//         return (<Login/>);
+//     }
+// }
+
+export default function(props){
+    console.log('authority');
+    return props.children;
+}
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,3 +1,8 @@
+/**
+ * title: 测试
+ * Routes: 
+ *  - ./src/layouts/Authority
+ */
 import router from 'umi/router';
 import BaseLayout from './baseLayout';
 import Login from './../pages/login';
@@ -5,12 +10,17 @@ import Util from './../util';
 
 const { handleLocalStorage } = Util;
 export default function(props){
+    console.log('layouts/index');
     const token = handleLocalStorage.getItem('_TOKEN');
     if(props.location.pathname==='/login'){
         return props.children;
     }else{
         if(token){
-            return (<BaseLayout>{ props.children }</BaseLayout>);
+            return (
+                <BaseLayout>
+                    { props.children }
+                </BaseLayout>
+            );
         }else{
             router.push('/login');
             return (<Login/>);
